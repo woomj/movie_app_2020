01ac98862963d463ae8cefc31ecb22a5293af3fb
woomj <speedcross99@naver.com>
2020-05-12 11:31:38 +0900

    movie not css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
-class App extends React.Component{
-  state= {
-    isLoading : true,
+class App extends React.Component {
+  state = {
+    isLoading: true,
     movies: []
   };
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  async componentDidMount() {
+    this.getMovies();
   }
   render() {
-    const{isLoading} =this.state;
-    return <div>{isLoading ? "Loading..." :"We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+       {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+        <Movie
+         key={movie.id}
+          id={movie.id}
+         year={movie.year}
+         title={movie.title}
+         summary={movie.summary}
+         poster={movie.medium_cover_image}
+         />
+        ))}
+     </div>
+    );
   }
 }
 
